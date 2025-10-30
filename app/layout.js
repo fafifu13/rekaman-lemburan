5bf60c76e8756329cf467aea327f32449191e211
fafifu13 <mintokoym@gmail.com>
2025-10-30 10:27:35 +0700

    Add RootLayout component with metadata

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Sistem Lemburan - KJPP AMANAH',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
